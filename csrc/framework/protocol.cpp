05deea016958b325b23d799178fcb794e9d583d8
MemScopeDeveloper <MemScopeDeveloper>
2024-11-06 09:37:45 +0800

    信息采集结构定义，实现protocol
    Match-id-0174d7eb96bd747b3db16309fe62e7cc377ef9e3

--- csrc/framework/protocol.cpp
+++ csrc/framework/protocol.cpp
@@ -1,16 +1,119 @@
 // Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
 #include "protocol.h"
+#include <mutex>
+#include <limits>
+#include "serializer.h"
 
 namespace Leaks {
 
+class Protocol::Extractor {
+public:
+    Extractor() = default;
+    ~Extractor() = default;
+    inline void Feed(const std::string &msg);
+    template<typename T>
+    inline bool Read(T &val);
+    inline bool Read(uint64_t size, std::string &buffer);
+
+private:
+    static constexpr uint64_t MAX_STRING_LEN = 1024UL;
+    inline void DropUsedBytes(void);
+
+private:
+    std::string bytes_;
+    std::string::size_type offset_{0UL};
+    std::mutex mutex_;
+};
+
+void Protocol::Extractor::Feed(const std::string &msg)
+{
+    std::lock_guard<std::mutex> guard(mutex_);
+    bytes_ += msg;
+}
+
+template <typename T>
+bool Protocol::Extractor::Read(T &val)
+{
+    std::string buffer;
+    if (!Read(sizeof(T), buffer)) {
+        return false;
+    }
+    return Deserialize<T>(buffer, val);
+}
+
+bool Protocol::Extractor::Read(uint64_t size, std::string &buffer)
+{
+    std::lock_guard<std::mutex> guard(mutex_);
+    uint64_t maxValue = std::numeric_limits<uint64_t>::max();
+    if (maxValue - offset_ <= size || offset_ + size > bytes_.size()) {
+        return false;
+    }
+    buffer = bytes_.substr(offset_, size);
+    offset_ += size;
+
+    if (offset_ > MAX_STRING_LEN) {
+        DropUsedBytes();
+    }
+    return true;
+}
+
+void Protocol::Extractor::DropUsedBytes(void)
+{
+    offset_ = std::min(offset_, bytes_.size());
+    bytes_ = bytes_.substr(offset_);
+    offset_ = 0UL;
+}
+
+Protocol::Protocol()
+{
+    extractor_ = std::make_shared<Extractor>();
+}
+
 void Protocol::Feed(std::string const &msg)
 {
+    if (msg.size() == 0) {
+        return;
+    }
+    extractor_->Feed(msg);
     return;
 }
 
 Packet Protocol::GetPacket(void)
 {
-    return Packet{};
+    static PacketHead head{PacketType::INVALID};
+    if (head.type == PacketType::INVALID) {
+        if (!extractor_->Read(head)) {
+            return Packet {};
+        }
+    }
+
+    Packet packet = GetPayLoad(head);
+    if (packet.GetPacketHead().type != PacketType::INVALID) {
+        head.type = PacketType::INVALID;
+    }
+
+    return packet;
+}
+
+Packet Protocol::GetPayLoad(PacketHead head)
+{
+    switch (head.type) {
+        case PacketType::RECORD:
+            return GetRecord();
+        case PacketType::INVALID:
+        default:
+            return Packet{};
+    }
+}
+
+Packet Protocol::GetRecord(void)
+{
+    EventRecord record{};
+    if (!extractor_->Read(record)) {
+        return Packet{};
+    }
+    auto packet = Packet(record);
+    return packet;
 }
 
 }
