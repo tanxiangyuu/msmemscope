4448021a5f199dcda558f7077227db64764a2be2
MemScopeDeveloper <MemScopeDeveloper>
2025-02-10 10:51:14 +0800

    1
    Match-id-619bd19b19622b9d05f0be2cb1214ebde2690d68

--- csrc/event_trace/mstx_hooks/mstx_inject.cpp
+++ csrc/event_trace/mstx_hooks/mstx_inject.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "log.h"
 #include "mstx_manager.h"
+#include "event_report.h"
 #include "kernel_hooks/runtime_hooks.h"
 
 namespace Leaks {
@@ -35,12 +36,12 @@ extern "C" int __attribute__((visibility("default"))) InitInjectionMstx(MstxGetM
     if (getFuncTable == nullptr ||
         getFuncTable(MstxFuncModule::MSTX_API_MODULE_CORE, &outTable, &outSize) != MSTX_SUCCESS ||
         outTable == nullptr) {
-        std::cout << "Failed to call getFuncTable" << std::endl;
+        ClientErrorLog("Failed to call getFuncTable");
         return MSTX_FAIL;
     }
 
     if (outSize != static_cast<unsigned int>(MstxFuncSeq::MSTX_FUNC_END)) {
-        std::cout << "OutSize is not equal to MSTX_FUNC_END, Failed to init mstx funcs." << std::endl;
+        ClientErrorLog("OutSize is not equal to MSTX_FUNC_END, Failed to init mstx funcs.");
         return MSTX_FAIL; // 1 : init failed
     }
     *(outTable[static_cast<unsigned int>(MstxFuncSeq::MSTX_FUNC_MARKA)]) =
