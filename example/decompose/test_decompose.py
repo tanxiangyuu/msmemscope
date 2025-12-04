bc4e19a057405ef650804ceebb69944e5a4f9cda
MemScopeDeveloper <MemScopeDeveloper>
2025-12-04 11:22:31 +0800

    ADD example
    Match-id-3b5f7e5cff6e33a4538c3dd27a15a679c0d9fc98

---
+++ example/decompose/test_decompose.py
@@ -0,0 +1,53 @@
+import torch
+import torch_npu
+import torch.nn as nn
+import torch.optim as optim
+import msmemscope
+import msmemscope.describe as describe
+
+
+def test():
+    msmemscope.config(analysis='decompose', data_format='csv', output='./output') # 将analysis设置为decompose进行内存拆解
+    device = torch.device('npu:0') # 可以更改为想要的卡号
+    torch.npu.set_device(device)
+
+
+    class SimpleModel(nn.Module):
+        def __init__(self):
+            super(SimpleModel, self).__init__()
+            self.linear = nn.Linear(10, 10)
+
+        def forward(self, x):
+            out = self.linear(x)
+            return out    
+    model = SimpleModel().to(device)
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    inputs = torch.randn(32, 10).to(device)
+    targets = torch.randn(32, 10).to(device)
+    
+    msmemscope.start() # 通过msmemscope.start()标识采集开始
+    with describe.describer(owner="train"): # 通过describe来标识代码块中的内存块，owner为自定义
+        for epoch in range(6):
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.requires_grad_(True)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if epoch % 2 == 0:
+                memory_allocated = torch.npu.memory_allocated(device) / (1024 ** 2)
+                max_memory_allocated = torch.npu.max_memory_allocated(device) / (1024 ** 2)
+                print(f"Epoch {epoch} : Current Memory Allocated = {memory_allocated:.2f} MB,", 
+                f"Max Memory Allocated = {max_memory_allocated:.2f} MB")
+            
+            torch.npu.empty_cache()
+    msmemscope.stop() # 通过msmemscope.stop()标识采集结束
+    
+
+def main():
+    test()
+    print("Test finished.")
+
+if __name__ == "__main__":
+    main()
