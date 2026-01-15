195f4e04f8f0377abc06b5782b6256c277bae423
tanxiangyuu <tanxiangyu2@huawei.com>
2026-01-15 17:11:47 +0800

    【bugfix】: 修复无法找到动态库文件错误
    Co-authored-by: tanxiangyuu<tanxiangyu2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !11 merge 0113master into master
    
    【bugfix】: 修复无法找到动态库文件错误
    
    Created-by: tanxiangyuu
    Commit-by: tanxiangyuu
    Merged-by: ascend-robot
    Description: 
    感谢您贡献的Pull Request！
    在提交之前，请务必阅读 [Contributing](https://gitcode.com/Ascend/msmemscope/blob/master/docs/zh/development_guide/contributing_guide.md)。
    
    ## 1. 修改描述
    <!--
    - 请明确说明您提交PR的变更内容。本部分旨在概述所做的变更，以及此PR是如何解决该问题的。请尽可能地提供有助于评审人员更高效、更快速完成检视审查的实用说明。
    - 请说明为何需要这些更改，例如具体的使用场景或bug描述。
    - 关联issue号(如果有)
    -->
    
    - **修改原因：**  memscope运行过程中报错“[ERROR] ../msmemscope/lib64/libascend_leaks.so: cannot open shared object file: No such file or directory"，但是功能正常。
    - **修改内容：**  在python模块中通过'ASCEND_HOME_PATH'导入环境变量，但是安装包后，set_env.sh 脚本会自动设置PYTHONPATH环境变量，此时python模块中的环境变量设置无必要。
    - [x] **是否关联Issue**
    ----
    
    ## 2. 功能验证
    <!--
    请确认CI已通过增量及存量的单元测试用例。
    如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤(最好提供完整的可复现的操作路径及关键截图)，以便Committer能够快速复现验证，也便于后续的维护。
    如果未添加测试，请说明未添加的原因，以及为何难添加测试。
    
    - [_] 功能自验
    - [_] 本地自验截图(涉及个人标识符等敏感信息请注意脱敏)
    - [_] 新增/变更内容是否已新增/适配UT测试用例看护
    -->
    
    - [ ] **功能自验**  
    - [ ] **本地自验用例截图**（请确保不体现个人信息）  
    - [ ] **是否具备UT/ST测试用例看护**
    ----
    
    ## 3. 代码检视
    - **要求：**
      - 合入代码大于 200 行，需三人以上会议检视。
      - 检视密度≥2个/100行。
      - 检视缺陷密度达不到要求的需给出说明。
    - [ ] **是否经过代码检视**  
    ----
    
    See merge request: Ascend/msmemscope!11

--- python/msmemscope/__init__.py
+++ python/msmemscope/__init__.py
@@ -17,20 +17,6 @@
 import os
 import ctypes
 
-# 首先设置环境变量
-ASCEND_HOME_PATH = os.getenv('ASCEND_HOME_PATH')
-LEAKS_LIB_PATH = ""
-if ASCEND_HOME_PATH:
-    LEAKS_LIB_PATH = os.path.join(ASCEND_HOME_PATH, "tools", "msmemscope", "lib64")
-
-# 加载依赖
-if LEAKS_LIB_PATH:
-    LEAKS_SO_PATH = os.path.join(LEAKS_LIB_PATH, "libascend_leaks.so")
-    try:
-        ctypes.CDLL(LEAKS_SO_PATH, mode=ctypes.RTLD_GLOBAL)
-    except OSError as e:
-        print(f"[ERROR] {e}")
-
 from ._msmemscope import _watcher
 from ._msmemscope import _tracer
 from ._msmemscope import start, stop, step, config
